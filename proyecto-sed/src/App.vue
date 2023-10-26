58c0e4f74f2d42a0a5fa38209eff55b088d5871e
EverthMartinez3002 <00156021@uca.edu.sv>
2023-10-26 09:27:57 -0600

    'estructura-front-end'

--- proyecto-sed/src/App.vue
+++ proyecto-sed/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+import HelloWorld from './components/HelloWorld.vue';
+export default{
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
